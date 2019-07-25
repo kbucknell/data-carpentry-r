83029382393cf72d38bc016f0e02f31c8e2686f8
Kirsten Bucknell <kirsten.bucknell@hdr.mq.edu.au>
2019-07-25 15:37:59 +1000

    area exercises

--- Scripts/Introduction.R
+++ Scripts/Introduction.R
@@ -5,4 +5,16 @@ library(tidyverse)
 
 print("hello world")
 
-area_hectares <- 1.0
+(area_hectares <- 1.0)
+1.0 -> area_hectares
+
+
+
+area_hectares <- 2.5      # leand area in hectares
+area_acres <- 2.47 * area_hectares  # convert to acres
+area_acres                 # print
+
+3.5 -> length
+4.0 -> width
+area <- length * width
+area
