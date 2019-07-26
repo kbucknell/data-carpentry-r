670a4e9aef6d31b57022136f90345d97c0e1f917
Kirsten Bucknell <kirsten.bucknell@hdr.mq.edu.au>
2019-07-26 11:23:33 +1000

    Factors - a limited number of items that occur repeatedly

--- Starting with data_Peter on Friday.R
+++ Starting with data_Peter on Friday.R
@@ -44,3 +44,29 @@ interviews[middle_idx, ]
 
 #q4
 interviews_head <- interviews[-(7:n_rows), ]
+
+#Factors, a specialised data type
+floor_type <- factor(c("earth", "cement", "cement", "earth"))
+levels(floor_type) #levels are the values a factor can have, R sorts alphabetically
+nlevels(floor_type) #number of levels
+
+#reordering levels so they're not alphabetical
+floor_type <- factor(c("earth", "cement", "cement", "earth"), 
+                     levels=c("earth", "cement"))
+#changing entry names
+levels(floor_type)[2] <-"brick"
+
+#turns factor into a character
+as.character(floor_type)
+
+#Pitfall for factors that look like numbers eg.year
+year_fct <- factor(c(1990, 1983, 1977, 1998, 1990))
+as.numeric(year_fct) # but it's turned the responses into indices/ categoriesie.1, 2, 3, 4, 1
+as.character(year_fct) #if character entries look like number, R treats them as numbers
+as.numeric(as.character(year_fct)) #this gives you years as numbers again
+
+#another way to convert factor numbers into other numbers, preferred but not very intuitive
+#exploit the indices and turn them into levels
+levels(year_fct)[year_fct]
+as.numeric(levels(year_fct))[year_fct]
+table(year_fct) #tells you how many of each years there are
